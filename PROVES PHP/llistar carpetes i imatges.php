1c0611d9dd22353c3bf75a5a17f277cc68ce51c9
Kitananji <adrisora7@gmail.com>
2014-11-26 16:54:03 +0100

    Boto facebook + PHP(unfinished)

--- PROVES PHP/llistar carpetes i imatges.php
+++ PROVES PHP/llistar carpetes i imatges.php
@@ -21,3 +21,54 @@ function listFolderFiles($dir){
 listFolderFiles('img');
 
 ?>
+
+
+
+
+
+
+ <?php
+                    function listFolderFiles($dir){
+                        $ffs = scandir($dir);
+                        $count = 1;
+                        foreach($ffs as $ff){
+                            if($ff != '.' && $ff != '..'){
+                                
+                                if(is_dir($dir.'/'.$ff)){
+                                    echo '<div class="col-md-4 col-sm-6 portfolio-item">';
+                                    echo '  <a href="#portfolioModal'.$count.'" class="portfolio-link" data-toggle="modal">';
+                                    echo '      <div class="portfolio-hover">';
+                                    echo '          <div class="portfolio-hover-content">';
+                                    echo '              <i class="fa fa-plus fa-3x"></i>';
+                                    echo '          </div>';
+                                    echo '      </div>';
+                                    echo '      <img src="img/portfolio/more.png" class="img-responsive" alt="">';
+                                    echo '  </a>';
+                                    echo '  <div class="portfolio-caption">';
+                                    echo '      <h4>'.$ff.'</h4>';
+                                    echo '      <p class="text-muted">'.$dir.'/'.$ff.'</p>';
+                                    echo '  </div>';
+                                    echo '</div>';
+                                }else{
+                                    echo '<div class="col-md-4 col-sm-6 portfolio-item">';
+                                    echo '  <a href="#portfolioModal'.$count.'" class="portfolio-link" data-toggle="modal">';
+                                    echo '      <div class="portfolio-hover">';
+                                    echo '          <div class="portfolio-hover-content">';
+                                    echo '              <i class="fa fa-plus fa-3x"></i>';
+                                    echo '          </div>';
+                                    echo '      </div>';
+                                    echo '      <img src="'.$dir.'/'.$ff.'" class="img-responsive" alt="">';
+                                    echo '  </a>';
+                                    echo '  <div class="portfolio-caption">';
+                                    echo '      <h4>'.$ff.'</h4>';
+                                    echo '      <p class="text-muted">'.$dir.'/'.$ff.'</p>';
+                                    echo '  </div>';
+                                    echo '</div>';
+                                }
+                                $count = $count + 1;
+                                
+                            }
+                        }
+                    }
+                    listFolderFiles('img/images');
+            ?>
