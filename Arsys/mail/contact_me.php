85d6cf60af1e12d50869c0097fa56904957809b4
Kitananji <kitananjikoga@gmail.com>
2015-03-07 19:55:46 +0100

    Arsys mail
    el contacte apunta a info@cangarganta.com

--- Arsys/mail/contact_me.php
+++ Arsys/mail/contact_me.php
@@ -16,7 +16,7 @@ $phone = $_POST['phone'];
 $message = $_POST['message'];
 	
 // Create the email and send the message
-$to = 'cangarganta@gmail.com'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
+$to = 'info@cangarganta.com'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
 $email_subject = "Website Contact Form:  $name";
 $email_body = "Has rebut un nou missatge provinent de la pagina web www.cangarganta.com.\n\n"."Aqui estan els detalls:\n\nNom: $name\n\nEmail: $email_address\n\nTelefon: $phone\n\nMissatge:\n$message";
 $headers = "From: noreply@cangarganta.com\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
